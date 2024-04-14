ecced0c7599398526517a6b50633b5018312de70
chenyuan33 <jz0622@hotmail.com>
2024-04-14 15:22:50 +0800

    Test: console.log( #headerBlanks .width, .height );

--- jses.js
+++ jses.js
@@ -1,4 +1,5 @@
 let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)), updateHeaderBlanks = () => {
 	document.getElementById("headerBlanks").style.width = document.getElementById("header").style.width;
 	document.getElementById("headerBlanks").style.height = document.getElementById("header").style.height;
+	console.log(document.getElementById("headerBlanks").style.width, document.getElementById("headerBlanks").style.height);
 };
