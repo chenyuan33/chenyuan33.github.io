aa75dc27ff0c0b5f3ef7debef7b1d5ad2902c1b2
chenyuan33 <115919187+chenyuan33@users.noreply.github.com>
2024-07-29 11:33:37 +0800

    Add light/dark mode.

--- jses.js
+++ jses.js
@@ -1,4 +1,38 @@
 let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)), updateHeaderBlanks = () => {
 	document.getElementById("headerBlanks").style.width = document.getElementById("header").offsetWidth + "px";
 	document.getElementById("headerBlanks").style.height = document.getElementById("header").offsetHeight + "px";
+	if (cookie_operator.get("lightMode") === undefined)
+	{
+		cookie_operator.set("lightMode", "dark");
+	}
+	else
+	{
+		if (cookie_operator.get("lightMode") === "light")
+		{
+			document.getElementById("lightSwitch").innerHTML = "light";
+			document.body.style.backgroundColor = "white";
+			document.body.style.color = "black";
+		}
+	}
+}, cookie_operator = {
+	get: (name) => {
+		let key_value = document.cookie.split("; ").find(str => str.startsWith(name + "="));
+		return key_value === undefined ? undefined : key_value.split("=")[1];
+	},
+	set: (name, value) => document.cookie = name + "=" + value + "; expires=" + new Date(Date.now() + 60 * 60 * 24 * 180).toUTCString() + "; path=/"
+}, switchLight = () => {
+	if (cookie_operator.get("lightMode") === "dark")
+	{
+		cookie_operator.set("lightMode", "light");
+		document.getElementById("lightSwitch").innerHTML = "light";
+		document.body.style.backgroundColor = "white";
+		document.body.style.color = "black";
+	}
+	else
+	{
+		cookie_operator.set("lightMode", "dark");
+		document.getElementById("lightSwitch").innerHTML = "dark";
+		document.body.style.backgroundColor = "black";
+		document.body.style.color = "white";
+	}
 };
