94c00db359bc58b5453112c9e0ec6621be2f4f0a
chenyuan33 <115919187+chenyuan33@users.noreply.github.com>
2024-10-02 07:43:37 +0800

    Add copyrights and an Easter Egg (Look at the console in any page in chenyuan33.github.io).

--- jses.js
+++ jses.js
@@ -1,6 +1,8 @@
-let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)), updateHeaderBlanks = () => {
+let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)), scripts = () => {
 	document.getElementById("headerBlanks").style.width = document.getElementById("header").offsetWidth + "px";
 	document.getElementById("headerBlanks").style.height = document.getElementById("header").offsetHeight + "px";
+	document.getElementById("footerBlanks").style.width = document.getElementById("footer").offsetWidth + "px";
+	document.getElementById("footerBlanks").style.height = document.getElementById("footer").offsetHeight + "px";
 	if (cookie_operator.get("lightMode") === undefined)
 	{
 		cookie_operator.set("lightMode", "dark");
@@ -14,6 +16,19 @@ let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)), updateHeade
 			document.body.style.color = "black";
 		}
 	}
+	console.log(`Welcome to chenyuan33.github.io!
+© 2024 @chenyuan33, All rights reserved.
+ /$$      /$$           /$$                                                     /$$                               /$$                                                                    /$$$$$$   /$$$$$$               /$$   /$$     /$$                 /$$          /$$           /$$
+| $$  /$ | $$          | $$                                                    | $$                              | $$                                                                   /$$__  $$ /$$__  $$             |__/  | $$    | $$                | $$         |__/          | $$
+| $$ /$$$| $$  /$$$$$$ | $$  /$$$$$$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$        /$$$$$$    /$$$$$$         /$$$$$$$| $$$$$$$   /$$$$$$  /$$$$$$$  /$$   /$$ /$$   /$$  /$$$$$$  /$$$$$$$ |__/  \ $$|__/  \ $$     /$$$$$$  /$$ /$$$$$$  | $$$$$$$  /$$   /$$| $$$$$$$     /$$  /$$$$$$ | $$
+| $$/$$ $$ $$ /$$__  $$| $$ /$$_____/ /$$__  $$| $$_  $$_  $$ /$$__  $$      |_  $$_/   /$$__  $$       /$$_____/| $$__  $$ /$$__  $$| $$__  $$| $$  | $$| $$  | $$ |____  $$| $$__  $$   /$$$$$/   /$$$$$/    /$$__  $$| $$|_  $$_/  | $$__  $$| $$  | $$| $$__  $$   | $$ /$$__  $$| $$
+| $$$$_  $$$$| $$$$$$$$| $$| $$      | $$  \ $$| $$ \ $$ \ $$| $$$$$$$$        | $$    | $$  \ $$      | $$      | $$  \ $$| $$$$$$$$| $$  \ $$| $$  | $$| $$  | $$  /$$$$$$$| $$  \ $$  |___  $$  |___  $$   | $$  \ $$| $$  | $$    | $$  \ $$| $$  | $$| $$  \ $$   | $$| $$  \ $$|__/
+| $$$/ \  $$$| $$_____/| $$| $$      | $$  | $$| $$ | $$ | $$| $$_____/        | $$ /$$| $$  | $$      | $$      | $$  | $$| $$_____/| $$  | $$| $$  | $$| $$  | $$ /$$__  $$| $$  | $$ /$$  \ $$ /$$  \ $$   | $$  | $$| $$  | $$ /$$| $$  | $$| $$  | $$| $$  | $$   | $$| $$  | $$    
+| $$/   \  $$|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$ | $$ | $$|  $$$$$$$        |  $$$$/|  $$$$$$/      |  $$$$$$$| $$  | $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$/|  $$$$$$//$$|  $$$$$$$| $$  |  $$$$/| $$  | $$|  $$$$$$/| $$$$$$$//$$| $$|  $$$$$$/ /$$
+|__/     \__/ \_______/|__/ \_______/ \______/ |__/ |__/ |__/ \_______/         \___/   \______/        \_______/|__/  |__/ \_______/|__/  |__/ \____  $$ \______/  \_______/|__/  |__/ \______/  \______/|__/ \____  $$|__/   \___/  |__/  |__/ \______/ |_______/|__/|__/ \______/ |__/
+                                                                                                                                                /$$  | $$                                                      /$$  \ $$                                                                 
+                                                                                                                                               |  $$$$$$/                                                     |  $$$$$$/                                                                 
+                                                                                                                                                \______/                                                       \______/                                                                  `);
 }, cookie_operator = {
 	get: (name) => {
 		let key_value = document.cookie.split("; ").find(str => str.startsWith(name + "="));
