80165b960e88e741d6bf6d08f297caa3dc5f402e
chenyuan33 <115919187+chenyuan33@users.noreply.github.com>
2025-11-02 11:39:34 +0800

    Update markdown (editor).

--- md.js
+++ md.js
@@ -5,27 +5,22 @@ let mdtohtml = (md) => {
 		if (line == "") {
 			return;
 		}
-		if (line.startsWith("#")) {
-			line = line
-				.replace(/^# (.*?)$/igm, "<h1>$1</h1>")
-				.replace(/^## (.*?)$/igm, "<h2>$1</h2>")
-				.replace(/^### (.*?)$/igm, "<h3>$1</h3>")
-				.replace(/^#### (.*?)$/igm, "<h4>$1</h4>")
-				.replace(/^##### (.*?)$/igm, "<h5>$1</h5>")
-				.replace(/^###### (.*?)$/igm, "<h6>$1</h6>");
+		line = line
+			.replace(/^# (.*?)$/igm, "</p><h1>$1</h1><p>")
+			.replace(/^## (.*?)$/igm, "</p><h2>$1</h2><p>")
+			.replace(/^### (.*?)$/igm, "</p><h3>$1</h3><p>")
+			.replace(/^#### (.*?)$/igm, "</p><h4>$1</h4><p>")
+			.replace(/^##### (.*?)$/igm, "</p><h5>$1</h5><p>")
+			.replace(/^###### (.*?)$/igm, "</p><h6>$1</h6><p>");
+		if (line.match(/^>[\s\S]*/igm)) {
+			let x = line.split(/^>/igm);
+			line = x[0];
+			x.reverse();
+			x.pop();
+			x.reverse();
+			line += `<blockquote>${mdtohtml(x.join(''))}</blockquote>`;
 		}
-		else if (line.match(/\n# (.*?)$/igm)) {
-			
-		}
-		else if (line.startsWith(">")) {
-			line = `<blockquote>${mdtohtml(line.replace(/^>/igm, ""), settings)}</blockquote>`;
-		}
-		else if (line.match(/^\d+\. .+?$/igm)) {
-
-		}
-		else {
-			line = `<p>${line}</p>`;
-		}
+		line = `<p>${line}</p>`;
 		line = line
 			.replace(/\x20\x20\n/igm, "<br />")
 			.replace(/\*\*\*([\s\S]*?)\*\*\*/igm, "<strong><em>$1</em></strong>")
@@ -34,7 +29,7 @@ let mdtohtml = (md) => {
 			.replace(/\b___([\s\S]*?)___\b/igm, "<strong><em>$1</em></strong>")
 			.replace(/\b__([\s\S]*?)__\b/igm, "<strong>$1</strong>")
 			.replace(/\b_([\s\S]*?)_\b/igm, "<em>$1</em>");
-		html += line;
+		html += '<p>' + line + '</p>';
 	});
-	return html;
+	return html.replace(/<p><\/p>/igm, "");
 }
